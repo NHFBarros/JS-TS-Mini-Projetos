b2e1b45cc005982817323b47c16e627167d136c0
nickolas <nickolasdesenvolvimento@gmail.com>
2026-03-22 23:52:51 -0300

    Adicionando api pokedex

--- JS-Pokédex/script.js
+++ JS-Pokédex/script.js
@@ -1,20 +1,20 @@
 async function fetchData(url) {
   try {
-    const response = await fetch(url); // Wait for the server response
+    const response = await fetch(url);
     
-    // Check if the request was successful
+
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
     
-    const data = await response.json(); // Parse the response body as JSON
+    const data = await response.json();
     console.log(data);
     return data;
     
   } catch (error) {
-    console.error('Error fetching data:', error); // Handle network errors or non-ok responses
+    console.error('Error fetching data:', error);
   }
 }
 
 // Example usage:
-fetchData('https://api.example.com');
+fetchData("https://pokeapi.co/api/v2/");
