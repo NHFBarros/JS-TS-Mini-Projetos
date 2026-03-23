6cf1bf3576188345906ef53ad103b1e9036ecafe
nickolas <nickolasdesenvolvimento@gmail.com>
2026-03-23 00:25:46 -0300

    pokedex

--- JS-Pokédex/script.js
+++ JS-Pokédex/script.js
@@ -1,7 +1,6 @@
 async function fetchData(url) {
   try {
-    const response = await fetch(url);
-    
+    const response = await fetch(url);    
 
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
@@ -16,5 +15,34 @@ async function fetchData(url) {
   }
 }
 
-// Example usage:
-fetchData("https://pokeapi.co/api/v2/");
+async function loadPokemon() {
+
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+  const data = await response.json()
+
+  const container = document.getElementById("pokedex")
+
+  data.results.forEach((pokemon, index) => {
+
+    const id = index + 1
+
+    const card = document.createElement("div")
+
+    card.className = "bg-white rounded-xl shadow-md p-4 text-center"
+
+    card.innerHTML = `
+    <div class="border w-30 py-5">
+      <img class="mx-auto"
+      src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png">
+      
+      <h2 class="capitalize font-semibold mt-2">${pokemon.name}</h2>
+      <p class="text-sm text-gray-500">#${id}</p>
+    </div>
+    `
+
+    container.appendChild(card)
+
+  })
+}
+
+loadPokemon()
